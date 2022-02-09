1b68672fd8b25c0f81e311f42d5b99d290ddbfa5
abhilashsn1234 <abhilashsneurope1234@gmail.com>
2022-02-09 10:22:39 +0530

    added scaffold

---
+++ test/system/guests_test.rb
@@ -0,0 +1,41 @@
+require "application_system_test_case"
+
+class GuestsTest < ApplicationSystemTestCase
+  setup do
+    @guest = guests(:one)
+  end
+
+  test "visiting the index" do
+    visit guests_url
+    assert_selector "h1", text: "Guests"
+  end
+
+  test "should create guest" do
+    visit guests_url
+    click_on "New guest"
+
+    fill_in "Guest name", with: @guest.guest_name
+    click_on "Create Guest"
+
+    assert_text "Guest was successfully created"
+    click_on "Back"
+  end
+
+  test "should update Guest" do
+    visit guest_url(@guest)
+    click_on "Edit this guest", match: :first
+
+    fill_in "Guest name", with: @guest.guest_name
+    click_on "Update Guest"
+
+    assert_text "Guest was successfully updated"
+    click_on "Back"
+  end
+
+  test "should destroy Guest" do
+    visit guest_url(@guest)
+    click_on "Destroy this guest", match: :first
+
+    assert_text "Guest was successfully destroyed"
+  end
+end
